0a3a92f48f10b64946124f0791a7967cfeb97d06
Seraj Ahmad <serajansari7704@gmail.com>
2025-07-19 18:11:10 +0530

    fix: use lowercase 'alammart' DB in connectDB

--- Backend/Database/db.js
+++ Backend/Database/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 export const connectDB=async()=>{
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+        dbName:"AALAM_MART"
+    }).then(()=>{
         console.log("Database connected successfully")
     }).catch((err)=>{
         console.log("Error connecting to database",err)
