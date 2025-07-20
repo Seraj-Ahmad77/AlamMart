1e89e4f71d225958966d5a360dc3f109ee701098
Seraj Ahmad <serajansari7704@gmail.com>
2025-07-20 20:35:25 +0530

    your meaningful commit message

--- Backend/server.js
+++ Backend/server.js
@@ -2,15 +2,27 @@
 import {app} from "./app.js"
 
 import {v2 as cloudinary} from "cloudinary"
-console.log("🌐 CLOUDINARY_NAME:", process.env.CLOUDINARY_NAME);
 
+
+// cloudinary.config({
+    
+//     cloud_name:process.env.CLOUDINARY_NAME,
+//     api_key:process.env.CLOUDINARY_API_KEY,
+//     api_secret:process.env.CLOUDINARY_SECRET_KEY
+// })
+import { v2 as cloudinary } from "cloudinary";
+console.log("Env check:", {
+  name: process.env.CLOUDINARY_NAME,
+  key: process.env.CLOUDINARY_API_KEY,
+  secret: process.env.CLOUDINARY_SECRET_KEY ? "loaded" : "missing"
+});
 
 cloudinary.config({
-    
-    cloud_name:process.env.CLOUDINARY_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_SECRET_KEY
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`)
