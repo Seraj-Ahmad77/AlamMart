df777865505655313318c24c484f1331add9dba9
Seraj Ahmad <serajansari7704@gmail.com>
2025-07-20 20:48:28 +0530

    Fix: cloudinary api_key env issue

--- Backend/server.js
+++ Backend/server.js
@@ -10,19 +10,12 @@ import {v2 as cloudinary} from "cloudinary"
 //     api_key:process.env.CLOUDINARY_API_KEY,
 //     api_secret:process.env.CLOUDINARY_SECRET_KEY
 // })
-import { v2 as cloudinary } from "cloudinary";
-console.log("Env check:", {
-  name: process.env.CLOUDINARY_NAME,
-  key: process.env.CLOUDINARY_API_KEY,
-  secret: process.env.CLOUDINARY_SECRET_KEY ? "loaded" : "missing"
-});
-
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET_KEY,
-});
-
+    
+    cloud_name:"derkxsflg",
+    api_key:"471352831668121",
+    api_secret:"CUXmBvl5JZveTLMU6HFtXVQ-tJ0"
+})
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`)
