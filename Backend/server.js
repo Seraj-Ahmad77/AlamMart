998d5e7f88eed20a197ec3e8e9c6268f82ec04ba
Seraj Ahmad <serajansari7704@gmail.com>
2025-07-20 20:23:07 +0530

    Your commit message here

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,9 @@ import {app} from "./app.js"
 import {v2 as cloudinary} from "cloudinary"
 
 cloudinary.config({
-    cloud_name:"derkxsflg",
-    api_key:"471352831668121",
-    api_secret:"CUXmBvl5JZveTLMU6HFtXVQ-tJ0"
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_SECRET_KEY
 })
 
 app.listen(process.env.PORT,()=>{
