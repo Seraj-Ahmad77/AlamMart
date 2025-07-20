0c6b340626cc18b0eb0a3326d5ceac744b15c5e3
Seraj Ahmad <serajansari7704@gmail.com>
2025-07-20 20:27:02 +0530

    fix: cloudinary env variables

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,11 @@
 import {app} from "./app.js"
 
 import {v2 as cloudinary} from "cloudinary"
+console.log("🌐 CLOUDINARY_NAME:", process.env.CLOUDINARY_NAME);
+
 
 cloudinary.config({
+    
     cloud_name:process.env.CLOUDINARY_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_SECRET_KEY
