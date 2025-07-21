9d93a21e4ecd1eaa2de08b2909f59380281d1fae
Seraj Ahmad <serajansari7704@gmail.com>
2025-07-21 10:06:50 +0530

    Fix: Cloudinary config updated and environment variables set

--- Backend/server.js
+++ Backend/server.js
@@ -4,17 +4,11 @@ import {app} from "./app.js"
 import {v2 as cloudinary} from "cloudinary"
 
 
-// cloudinary.config({
-    
-//     cloud_name:process.env.CLOUDINARY_NAME,
-//     api_key:process.env.CLOUDINARY_API_KEY,
-//     api_secret:process.env.CLOUDINARY_SECRET_KEY
-// })
 cloudinary.config({
     
-    cloud_name:"derkxsflg",
-    api_key:"471352831668121",
-    api_secret:"CUXmBvl5JZveTLMU6HFtXVQ-tJ0"
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_SECRET_KEY
 })
 
 app.listen(process.env.PORT,()=>{
